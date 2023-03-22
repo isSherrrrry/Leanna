b175b5edd12b1c3c5c712530f2f196abe3ceb0bb
ericxue64 <101534764+ericxue64@users.noreply.github.com>
2023-03-22 17:23:14 -0400

    eric's update

--- src/LEANNA.py
+++ src/LEANNA.py
@@ -1,27 +1,123 @@
+import json
 import pickle
 import os.path
+from json import JSONDecodeError
+
 from emora_stdm import DialogueFlow
 from emora_stdm import Macro, Ngrams
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Callable
 import time
 import re
+import openai
+
+PATH_API_KEY = 'resources/openai_api.txt'
+openai.api_key_path = PATH_API_KEY
+
 
 def visits() -> DialogueFlow:
     transition_visit = {
         'state': 'start',
-        '`Hi, I\'m Leanna, your personal start-up consultant. May I know the name of future business tycoon?`': {
-
+        '`Hi, I\'m Leanna, your personal start-up consultant. At #TIME , I had the pleasure to meet '
+        ' With time as our guide, our encounter was meant to be. May I know the name of future business tycoon?`': {
+            '#GET_NAME': {
+                '``'
+            }
         }
     }
 
-
-
-
     macros = {
-
+        'GET_Call_NAME': MacroNLG(),
+        'SET_CALL_NAMES': MacroGPTJSON(
+            'How does the speaker want to be called?',
+            {V.call_names.name: ["Mike", "Michael"]}),
+        'TIME': MacroTime()
     }
 
     df = DialogueFlow('start', end_state='end')
     df.load_transitions(transition_visit)
     df.add_macros(macros)
     return df
+
+
+def gpt_completion(input: str, regex: Pattern = None) -> str:
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{'role': 'user', 'content': input}]
+    )
+    output = response['choices'][0]['message']['content'].strip()
+
+    if regex is not None:
+        m = regex.search(output)
+        output = m.group().strip() if m else None
+
+    return output
+
+
+class MacroGPTJSON(Macro):
+    def __init__(self, request: str, full_ex: Dict[str, Any], empty_ex: Dict[str, Any] = None,
+                 set_variables: Callable[[Dict[str, Any], Dict[str, Any]], None] = None):
+        self.request = request
+        self.full_ex = json.dumps(full_ex)
+        self.empty_ex = '' if empty_ex is None else json.dumps(empty_ex)
+        self.check = re.compile(regexutils.generate(full_ex))
+        self.set_variables = set_variables
+
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+        examples = f'{self.full_ex} or {self.empty_ex} if unavailable' if self.empty_ex else self.full_ex
+        prompt = f'{self.request} Respond in the JSON schema such as {examples}: {ngrams.raw_text().strip()}'
+        output = gpt_completion(prompt)
+        if not output: return False
+
+        try:
+            d = json.loads(output)
+        except JSONDecodeError:
+            print(f'Invalid: {output}')
+            return False
+
+        if self.set_variables:
+            self.set_variables(vars, d)
+        else:
+            vars.update(d)
+
+        return True
+
+
+class MacroNLG(Macro):
+    def __init__(self, generate: Callable[[Dict[str, Any]], str]):
+        self.generate = generate
+
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+        return self.generate(vars)
+
+
+class MacroTime(Macro):
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[str]):
+        current_time = time.strftime("%H:%M")
+        return current_time
+
+
+def save(df: DialogueFlow, varfile: str):
+    d = {k: v for k, v in df.vars().items() if not k.startswith('_')}
+    pickle.dump(d, open(varfile, 'wb'))
+
+
+def load(df: DialogueFlow, varfile: str):
+    d = pickle.load(open(varfile, 'rb'))
+    df.vars().update(d)
+    df.run()
+    save(df, varfile)
+
+
+if __name__ == '__main__':
+    df = visits()
+    # run save() for the first time,
+    # run load() for subsequent times
+
+    path = '../../resources/visits.pkl'
+
+    check_file = os.path.isfile(path)
+    if check_file:
+        load(df, '../../resources/visits.pkl')
+    else:
+        df.run()
+        save(df, '../../resources/visits.pkl')
