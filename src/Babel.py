0607979d2f5c9aed4e6fc936acef6549cfa675c6
sherry_yijun <sherrylyj@outlook.com>
2023-04-23 18:16:26 -0400

    update

--- src/Babel.py
+++ src/Babel.py
@@ -20,19 +20,19 @@ openai.api_key_path = PATH_API_KEY
 def visits() -> DialogueFlow:
     transition_greeting = {
         'state': 'start',
-        '`Hi, my name is Bomo. I am here to talk about movies. \n '
-        'What\'s your name? \n '
+        '`Hi, my name is Bomo. I am here to talk about movies.\n'
+        'What\'s your name? \n'
         'By the way, although I will be extremely sad:( '
         'but you can quit anytime by typing quit to me.`': {
             '#SET_CALL_NAMES': {
                 '#USER_GREETING `Do you like watching movies?`': {
-                    '#YESNO': {
-                        '#IF($yesno=yes) `Great! I like watching movies too. \n '
+                    '#SET_YESNO': {
+                        '#IF($yesno=yes) `Great! I like watching movies too. \n'
                         'I recently just watched a movie called '
-                        'Babel. `': 'babel_start',
-                        '`Okay, I am actually a big fan of movies. \n '
+                        'Babel.`': 'babel_start',
+                        '`Okay, I am actually a big fan of movies. \n'
                         'I just watched a movie called Babel and like it very'
-                        ' much!`': {
+                        ' much!\n`': {
                             'score': 0.1,
                             'state': 'babel_start'
                         }
@@ -54,17 +54,27 @@ def visits() -> DialogueFlow:
 
     transition_babel = {
         'state': 'babel_start',
-        '`I know you have watched the movie before. \n '
+        '`I know you have watched the movie before too. \n'
         'I like the movie very much, '
-        'especially the background story of Babel Tower. \n'
-        ' Do you know the story of Babel Tower?`': {
-            '#YESNO': {
-                '#IF($yesno=yes) `I\'m glad you know it too. \n '
+        'especially the background story of Babel Tower.\n'
+        'Do you know the story of Babel Tower?`': {
+            '#SET_YESNO': {
+                '#IF($yesno=yes) `I\'m glad you know it too. \n'
                 'I like the concept of translating up and down too.`': 'updown',
-                '`Elaborate the story of Babel Tower. \n'
-                ' I like the concept of translating up and down too.`': {
+                '`Here\'s the story of Babel Tower: \n'
+                'The story of the Babel tower stems from the bible in the book of Genesis. \n'
+                'At the beginning, humanity all spoke one language and because of their ease of communication,\n'
+                'they decided to work together to build a tower that reaches to the heavens in order to '
+                'make a name for themselves and avoid being scattered across the earth. \n'
+                'God saw such work as defiance and is not pleased. \n'
+                'God decides to revoke their universal language by causing them to speak different tongues '
+                'so they cannot understand each other. \n'
+                'This then leads to humanity being dispersed across the world in different nations.\n'
+                'How do you feel about it?`': {
                     'score': 0.1,
-                    'state': 'updown'
+                    'error': {
+                        'Sure. I also like the concept of translating up and down too.': 'updown'
+                    }
                 }
             }
         }
@@ -72,22 +82,55 @@ def visits() -> DialogueFlow:
 
     transition_updown = {
         'state': 'updown',
-        '`You know, in translation, there is a concept called "translating up/down". \n '
+        '`You know, in translation, there is a concept called "translating up/down". \n'
         'Translating up is from less'
-        ' powerful language to more powerful language. \n'
-        ' Translating down is the other way around. Do you notice any '
+        ' powerful language to more powerful language.\n'
+        ' Translating down is the other way around.\nIt is taking a sophisticated language in terms of language complexity '
+        'and technicality and simplifying it,\n'
+        'missing the key hints of other meanings to become a literal translation.\n'
+        'Do you notice any '
         'scenes where translating down affect?`': {
             '#GET_KNOW_UPDOWN': {
-                '#IF($user_know=yes) `THE PHRASE`': 'quote',
-                '`THE PHRASE. Can you think of any similar scenes in the movie?`': {
+                '#IF($user_know=yes) `Cool! I also know an example too. \n'
+                'It is a conversation between Nathan Gamble and Gael Garcia Bernal.\n'
+                'Nathan said that \"My mom said Mexico is dangerous.\" and Gael said  \"Yes, it\'s full of Mexicans.\"\n'
+                'This is an example of translating down because with Nathan’s quote, '
+                'he is saying something that reflects American viewpoints of Mexico. \n'
+                'Danger holds many meanings in English, not only is it a word that '
+                'describes the potential of not being safe, \n'
+                'it is also a word rooted in negative meaning and in a way, racism. \n'
+                'However, when a Mexican interprets it, only the central meaning is kept without '
+                'the complexity of connections to a negative word and also racism. \n'
+                'Therefore, a \"weird\" interaction occurs between the American child who is quite serious in his statement '
+                'and the Mexican relative who is quite comical in his response.\n'
+                'I also know a bunch of good quotes in the movie!\n`': 'quote',
+                '`Let me give you an example. \n'
+                'It is a conversation between Nathan Gamble and Gael Garcia Bernal.'
+                'Nathan said that \"My mom said Mexico is dangerous.\" and Gael said  \"Yes, it\'s full of Mexicans.\"\n'
+                'This is an example of translating down because with Nathan’s quote, '
+                'he is saying something that reflects American viewpoints of Mexico. \n'
+                'Danger holds many meanings in English, not only is it a word that '
+                'describes the potential of not being safe, \n'
+                'it is also a word rooted in negative meaning and in a way, racism. \n'
+                'However, when a Mexican interprets it, only the central meaning is kept without '
+                'the complexity of connections to a negative word and also racism. \n'
+                'Therefore, a \“weird\” interaction occurs between the American child who is quite serious in his statement '
+                'and the Mexican relative who is quite comical in his response.\n'
+                'Can you think of any similar scenes in the movie?`': {
                     'score': 0.2,
-                    '#YESNO': {
+                    '#SET_YESNO': {
                         '#IF(#yesno=yes) `Wow, you are absolutely right. The power of the language definitely '
                         'plays a role in here. This conversation is interesting. I happened to find some quotes '
                         'related to this movie.`': 'quote',
                         '`It’s ok, I have some interesting quotes related to the movie that you might find '
                         'interesting. I want to know how you think about them.`': {
                             'score': 0.2,
+                            'state': 'quote'
+                        }
+                    },
+                    'error': {
+                        '`It’s ok, I have some interesting quotes related to the movie that you might find '
+                        'interesting. I want to know how you think about them.\n`': {
                             'state': 'quote'
                         }
                     }
@@ -105,15 +148,15 @@ def visits() -> DialogueFlow:
             '#QUOTE_ANS': {
                 '#IF(yesno=yes) `Yeah, I totally agree` #GET_RESPONSE `Would you like another quote?`': {
                     'state': 'more_quote',
-                    '#YESNO': {
-                        '#IF(yesno=yes) `Sure, here is another one.`': 'quote',
+                    '#SET_YESNO': {
+                        '#IF(yesno=yes) `Sure, here is another one.\n`': 'quote',
                         'Okay': {
                             'score': 0.1,
                             'state': 'depart'
                         }
                     }
                 },
-                '`It\'s ok if you cannot find any.` #GET_RESPONSE `Would you like another quote?`': 'more_quote'
+                '`It\'s ok if you cannot find any.\n` #GET_RESPONSE `Would you like another quote?`': 'more_quote'
             }
         }
     }
@@ -127,7 +170,7 @@ def visits() -> DialogueFlow:
         'SET_CALL_NAMES': MacroGPTJSON(
             'What is speaker\'s name?, respond in lower case',
             {V.call_names.name: ["mike"]}, V.call_names.name, True),
-        'YESNO': MacroGPTJSON(
+        'SET_YESNO': MacroGPTJSON(
             'The speaker is answering a yes/no question. Categorize his response into yes or no',
             {V.yesno.name: "yes"}, V.yesno.name, True),
         'GET_KNOW_UPDOWN': MacroGPTJSON(
@@ -147,6 +190,7 @@ def visits() -> DialogueFlow:
     df.load_transitions(transition_greeting)
     df.load_transitions(transition_babel)
     df.load_transitions(transition_updown)
+    df.load_transitions(transition_quotes)
     df.load_transitions(transition_depart)
     df.add_macros(macros)
     return df
@@ -193,7 +237,6 @@ class MacroGPTJSON(Macro):
             if isinstance(ls, list):
                 vars[self.field] = ls[random.randrange(len(ls))]
 
-        print(vars) # for debug
 
         return True
 
